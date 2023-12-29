0bc4b4cd02707d437817d49b5115060d19cbf5cd
BigIskander <bigiskander@gmail.com>
2023-12-29 17:04:08 +0300

    More testing. Still slow...

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,13 +8,28 @@ use base64::decode;
 use image::load_from_memory;
 use rusty_tesseract::{Args, Image, image_to_string};
 use std::collections::HashMap;
+use tempfile::tempfile;
 
 #[tauri::command]
 fn recognize_text(base_64_image: String) -> String {
     // println!("{base_64_image}");
     let vec8_image = decode(base_64_image).unwrap();
     let dynamic_image = load_from_memory(&vec8_image).unwrap();
+    //
+    let tempfile = tempfile::Builder::new()
+            .prefix("rusty-tesseract")
+            .suffix(".png")
+            .tempfile()
+            .unwrap();
+    let path = tempfile.path();
+    dynamic_image.save(path); // saving image to temp path is slow...
+
+    // let path_str = path;
+    // println!("{path_str}");
+
+    //
     // let image = Image::from_dynamic_image(&dynamic_image).unwrap(); // slow function
+    // let image = Image::from_path(path).unwrap();
     return  "respond...".to_string();
     // let my_args = Args {
     //     lang: "chi_all".to_string(),
