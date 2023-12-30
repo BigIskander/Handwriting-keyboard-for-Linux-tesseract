f758e006ae29c712de2fd9875b985d0881f1bf94
BigIskander <bigiskander@gmail.com>
2023-12-31 02:01:01 +0300

    Add CLI options.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,14 +4,40 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
+use std::sync::Mutex;
 use base64::decode;
 use std::io::Write;
 use std::process::{Command, Stdio};
 
+// languages traindata dir for tesseract [value read from CLI]
+static TESSDATA_DIR: Mutex<String> = {
+    let dir = String::new();
+    Mutex::new(dir)
+};
+
+// language for recognition [value read from CLI]
+static LANG: Mutex<String> = {
+    let lang = String::new();
+    Mutex::new(lang)
+};
+
 #[tauri::command]
 fn recognize_text(base_64_image: String) -> Result<String, String> {
     let vec8_image = decode(base_64_image).unwrap();
-    let mut comm_exec = Command::new("tesseract").args(["-l", "chi_all", "--dpi", "96", "--psm", "13", "--oem", "3", "-", "stdout"]).stdin(Stdio::piped()).stderr(Stdio::piped()).stdout(Stdio::piped()).spawn().map_err(|err| "Tesseract api call, Error: ".to_string() + &err.to_string())?;
+    // working with CLI parameters
+    let cli_lang = LANG.lock().unwrap();
+    let mut lang = "chi_all".to_string();
+    if !cli_lang.is_empty() { 
+        lang = cli_lang.to_string(); 
+    }
+    let mut comm_args = ["-l", &lang, "--dpi", "96", "--psm", "13", "--oem", "3", "-", "stdout"].to_vec();
+    let cli_tessdata_dir = TESSDATA_DIR.lock().unwrap();
+    if !cli_tessdata_dir.is_empty() {
+        comm_args.insert(0, "--tessdata-dir");
+        comm_args.insert(1, &cli_tessdata_dir);
+    }
+    // call tesseract, send image via stdio and get results
+    let mut comm_exec = Command::new("tesseract").args(comm_args).stdin(Stdio::piped()).stderr(Stdio::piped()).stdout(Stdio::piped()).spawn().map_err(|err| "Tesseract api call, Error: ".to_string() + &err.to_string())?;
     let comm_stdin = comm_exec.stdin.as_mut().unwrap();
     comm_stdin.write_all(&vec8_image).unwrap();
     drop(comm_stdin);
@@ -36,6 +62,22 @@ fn paste_text() -> Result<(), String> {
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            match app.get_cli_matches() {
+                Ok(matches) => {
+                    let cli_tessdata_dir = &matches.args.get("tessdata-dir").expect("Error reading CLI.").value;
+                    if cli_tessdata_dir.is_string() {
+                        TESSDATA_DIR.lock().unwrap().insert_str(0, cli_tessdata_dir.as_str().expect("Error reading CLI."));
+                    }
+                    let cli_lang = &matches.args.get("lang").expect("Error reading CLI.").value;
+                    if cli_lang.is_string() {
+                        LANG.lock().unwrap().insert_str(0, cli_lang.as_str().expect("Error reading CLI."));
+                    }
+                }
+                Err(_) => {}
+            }
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![recognize_text, paste_text])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
