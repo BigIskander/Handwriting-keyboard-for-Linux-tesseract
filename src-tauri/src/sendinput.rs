c15cb7a12fe0d6b4235235cd03cb027057158b1b
BigIskander <bigiskander@gmail.com>
2025-03-04 19:50:03 +0300

    Implementing --debug option in sendinput.rs

--- src-tauri/src/sendinput.rs
+++ src-tauri/src/sendinput.rs
@@ -5,7 +5,10 @@ use crate::DEBUG;
 use crate::SKIP_TASKBAR;
 
 pub fn write_text(text: String, in_focus: bool, use_clipboard: bool) -> Result<(), String> {
-    // return  Err("test error".to_string());
+    let debug = DEBUG.lock().unwrap();
+    if !debug.is_empty() {
+        println!("Writing text using xdotools.");
+    }
     let mut comm_args = [].to_vec();
     let skip_taskbar = SKIP_TASKBAR.lock().unwrap();
     if in_focus && skip_taskbar.is_empty() {
@@ -23,28 +26,37 @@ pub fn write_text(text: String, in_focus: bool, use_clipboard: bool) -> Result<(
     } else {
         comm_args.append(&mut ["type", "--delay", "300", &text].to_vec());
     }
+    if !debug.is_empty() {
+        println!("Executing command: xdotool");
+        print!("Command args: ");
+        println!("{:?}", comm_args);
+    }
     let comm_exec = Command::new("xdotool")
         .args(comm_args)
         .output()
         .map_err(|err| "Xdotool call, Error: ".to_string() + &err.to_string())?;
     let comm_output_stderr = String::from_utf8_lossy(&comm_exec.stderr).to_string();
     if comm_output_stderr != "" {
-        let debug = DEBUG.lock().unwrap();
-        if !debug.is_empty() {
-            println!("Xdotool call, Error: {}", &comm_output_stderr);
-        }
         return Err("Xdotool call, Error: ".to_string() + &comm_output_stderr);
     }
     return Ok(());
 }
 
 pub fn alt_tab() -> Result<(), String> {
-    // return Err("test error".to_string());
+    let debug = DEBUG.lock().unwrap();
+    if !debug.is_empty() {
+        println!("Triggering alt+Tab keypress using xdotools.");
+    }
     let comm_exec = Command::new("xdotool")
         .args(["key", "--delay", "100", "alt+Tab"])
         .output()
         .map_err(|err| "Xdotool call, Error: ".to_string() + &err.to_string())
         .unwrap();
+    if !debug.is_empty() {
+        println!("Executing command: xdotool");
+        print!("Command args: ");
+        println!("{:?}", ["key", "--delay", "100", "alt+Tab"]);
+    }
     let comm_output_stderr = String::from_utf8_lossy(&comm_exec.stderr).to_string();
     if comm_output_stderr != "" {
         let debug = DEBUG.lock().unwrap();
