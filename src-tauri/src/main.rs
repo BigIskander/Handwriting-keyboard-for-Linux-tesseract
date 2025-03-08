7dda4ab3a74efabbbbebdc426eb8455d690ab610
BigIskander <bigiskander@gmail.com>
2025-03-08 22:55:37 +0300

    Small change.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -169,7 +169,7 @@ fn main() {
                                     1) write text in the canvas by using mouse or stylus (on graphical tablet) \n\
                                     2) press recognize button \n\
                                     3) press to recognized text, programm will type this text \n\
-                                    \t or copy to clipboard and paste by triggering ctrl+V (or shift+ctrl+V) keypress
+                                    \t or copy to clipboard and paste by triggering ctrl+V (or shift+ctrl+V) keypress \n\
                                     \n\
                                     To recognize handwritten pattern program uses OCR engine. \n\
                                     At the moment programm supports 2 OCR engines, which is: \n\
